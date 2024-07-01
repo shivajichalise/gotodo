82b8a0fba9c8e48d06931571d1a05efe0f146eff
Shivaji Chalise <respond2shivaji@gmail.com>
2024-07-01 12:29:15 +0545

    add todo

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module gotodo
 go 1.22.4
 
 require (
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -22,24 +23,47 @@ type Todo struct {
 	Todo string `json:"todo"`
 }
 
-var todos []Todo
-
-func add_todo(w http.ResponseWriter, _ *http.Request) {
+func AddTodoHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	todo := Todo{Id: "1", Todo: "Test"}
-	todos = append(todos, todo)
-	log.Printf("INFO: add todo is hit\n")
-	json.NewEncoder(w).Encode(todos)
-}
 
-func get_todos(w http.ResponseWriter, _ *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+	var todo Todo
+
+	err := json.NewDecoder(r.Body).Decode(&todo)
+	if err != nil {
+		log.Fatalf("ERROR: could not read request payload: %v", err.Error())
+	}
 
 	//---------------------SQL start-----------------------------------------------//
-	sql_stmt := `SELECT * from todos;`
+	uuid, err := uuid.NewUUID()
+	if err != nil {
+		log.Fatalf("ERROR: could not generate a UUID: %v", err.Error())
+	}
+	todo.Id = uuid.String()
+
+	stmt, err := db.Prepare(`INSERT INTO todos(id, todo) VALUES(?, ?)`)
+	if err != nil {
+		log.Fatalf("ERROR: could not prepare query: %v", err.Error())
+	}
+	defer stmt.Close()
+
+	_, query_err := stmt.Exec(todo.Id, todo.Todo)
+	if query_err != nil {
+		log.Fatalf("ERROR: could not add new todo: %v", err.Error())
+	}
+	//---------------------SQL end-----------------------------------------------//
+
+	json.NewEncoder(w).Encode(todo)
+}
+
+func GetTodoHandler(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var todos []Todo
+
+	//---------------------SQL start-----------------------------------------------//
+	sql_stmt := `SELECT * FROM todos;`
 	result, err := db.Query(sql_stmt)
 	if err != nil {
-		log.Fatalf("ERROR: could not fetch todos: %v", err)
+		log.Fatalf("ERROR: could not fetch todos: %v", err.Error())
 	}
 	defer result.Close()
 
@@ -49,7 +73,7 @@ func get_todos(w http.ResponseWriter, _ *http.Request) {
 
 		scan_err := result.Scan(&id, &todo)
 		if scan_err != nil {
-			log.Fatalf("ERROR: could not extract todos data: %v", err)
+			log.Fatalf("ERROR: could not extract todos data: %v", err.Error())
 		}
 
 		todos = append(todos, Todo{id, todo})
@@ -57,24 +81,24 @@ func get_todos(w http.ResponseWriter, _ *http.Request) {
 
 	result_err := result.Err()
 	if result_err != nil {
-		log.Fatalf("ERROR: cannot complete the iteration: %v", result_err)
+		log.Fatalf("ERROR: cannot complete the iteration: %v", result_err.Error())
 	}
 	//---------------------SQL end-----------------------------------------------//
 
 	json.NewEncoder(w).Encode(todos)
 }
 
-func update_todo(w http.ResponseWriter, _ *http.Request) {
+func UpdateTodoHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "Update todo")
 	log.Printf("INFO: update todo is hit\n")
 }
 
-func delete_todo(w http.ResponseWriter, _ *http.Request) {
+func DeleteTodoHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "Delete todo")
 	log.Printf("INFO: delete todo is hit\n")
 }
 
-func mark_todo_as_complete(w http.ResponseWriter, _ *http.Request) {
+func MarkTodoCompleteHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprint(w, "Complete todo")
 	log.Printf("INFO: mark todo complete is hit\n")
 }
@@ -83,16 +107,16 @@ func main() {
 	var db_err error
 	db, db_err = sql.Open("sqlite3", "./gotodo.db")
 	if db_err != nil {
-		log.Fatalf("ERROR: cannot connect to database: %v", db_err)
+		log.Fatalf("ERROR: cannot connect to database: %v", db_err.Error())
 	}
 	defer db.Close()
 
 	router := mux.NewRouter()
-	router.HandleFunc("/api/todos", get_todos).Methods("GET")
-	router.HandleFunc("/api/todos", add_todo).Methods("POST")
-	router.HandleFunc("/api/todos/{todo}", update_todo).Methods("PUT")
-	router.HandleFunc("/api/todos/{todo}", delete_todo).Methods("DELETE")
-	router.HandleFunc("/api/todos/{todo}", mark_todo_as_complete).Methods("PATCH")
+	router.HandleFunc("/api/todos", GetTodoHandler).Methods("GET")
+	router.HandleFunc("/api/todos", AddTodoHandler).Methods("POST")
+	router.HandleFunc("/api/todos/{todo}", UpdateTodoHandler).Methods("PUT")
+	router.HandleFunc("/api/todos/{todo}", DeleteTodoHandler).Methods("DELETE")
+	router.HandleFunc("/api/todos/{todo}", MarkTodoCompleteHandler).Methods("PATCH")
 
 	log.Printf("INFO: server is listening on port: %v\n", PORT)
 
