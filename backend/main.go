2716cbe9b63cdd6535b281cb3aea89ebd7d4fca0
Shivaji Chalise <respond2shivaji@gmail.com>
2024-07-01 13:00:01 +0545

    mark todo complete and few corrections

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -34,25 +33,25 @@ func AddTodoHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&todo)
 	if err != nil {
-		log.Fatalf("ERROR: could not read request payload: %v", err.Error())
+		log.Fatalf("ERROR: could not read request payload: %s", err.Error())
 	}
 
 	//---------------------SQL start-----------------------------------------------//
 	uuid, err := uuid.NewUUID()
 	if err != nil {
-		log.Fatalf("ERROR: could not generate a UUID: %v", err.Error())
+		log.Fatalf("ERROR: could not generate a UUID: %s", err.Error())
 	}
 	todo.Id = uuid.String()
 
 	stmt, err := db.Prepare(`INSERT INTO todos(id, todo) VALUES(?, ?)`)
 	if err != nil {
-		log.Fatalf("ERROR: could not prepare query: %v", err.Error())
+		log.Fatalf("ERROR: could not prepare query: %s", err.Error())
 	}
 	defer stmt.Close()
 
 	_, query_err := stmt.Exec(todo.Id, todo.Todo)
 	if query_err != nil {
-		log.Fatalf("ERROR: could not add new todo: %v", err.Error())
+		log.Fatalf("ERROR: could not add new todo: %s", err.Error())
 	}
 	//---------------------SQL end-----------------------------------------------//
 
@@ -67,7 +66,7 @@ func GetTodoHandler(w http.ResponseWriter, _ *http.Request) {
 	sql_stmt := `SELECT * FROM todos;`
 	result, err := db.Query(sql_stmt)
 	if err != nil {
-		log.Fatalf("ERROR: could not fetch todos: %v", err.Error())
+		log.Fatalf("ERROR: could not fetch todos: %s", err.Error())
 	}
 	defer result.Close()
 
@@ -77,7 +76,7 @@ func GetTodoHandler(w http.ResponseWriter, _ *http.Request) {
 
 		scan_err := result.Scan(&id, &todo)
 		if scan_err != nil {
-			log.Fatalf("ERROR: could not extract todos data: %v", err.Error())
+			log.Fatalf("ERROR: could not extract todos data: %s", err.Error())
 		}
 
 		todos = append(todos, Todo{id, todo})
@@ -85,7 +84,7 @@ func GetTodoHandler(w http.ResponseWriter, _ *http.Request) {
 
 	result_err := result.Err()
 	if result_err != nil {
-		log.Fatalf("ERROR: cannot complete the iteration: %v", result_err.Error())
+		log.Fatalf("ERROR: cannot complete the iteration: %s", result_err.Error())
 	}
 	//---------------------SQL end-----------------------------------------------//
 
@@ -101,18 +100,18 @@ func UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&todo)
 	if err != nil {
-		log.Fatalf("ERROR: could not read request payload: %v", err.Error())
+		log.Fatalf("ERROR: could not read request payload: %s", err.Error())
 	}
 
 	stmt, err := db.Prepare(`UPDATE todos SET todo = ? WHERE id = ?`)
 	if err != nil {
-		log.Fatalf("ERROR: could not prepare query: %v", err.Error())
+		log.Fatalf("ERROR: could not prepare query: %s", err.Error())
 	}
 	defer stmt.Close()
 
 	_, query_err := stmt.Exec(todo.Todo, todo_id)
 	if query_err != nil {
-		log.Fatalf("ERROR: could not update todo: %v", err.Error())
+		log.Fatalf("ERROR: could not update todo: %s", err.Error())
 	}
 
 	response := Response{Message: "Todo updated successfully."}
@@ -126,43 +125,66 @@ func DeleteTodoHandler(w http.ResponseWriter, r *http.Request) {
 
 	stmt, err := db.Prepare(`DELETE FROM todos WHERE id = ?`)
 	if err != nil {
-		log.Fatalf("ERROR: could not prepare query: %v", err.Error())
+		log.Fatalf("ERROR: could not prepare query: %s", err.Error())
 	}
 	defer stmt.Close()
 
 	_, query_err := stmt.Exec(todo_id)
 	if query_err != nil {
-		log.Fatalf("ERROR: could not delete todo: %v", err.Error())
+		log.Fatalf("ERROR: could not delete todo: %s", err.Error())
 	}
 
 	response := Response{Message: "Todo deleted successfully."}
 	json.NewEncoder(w).Encode(response)
 }
 
-func MarkTodoCompleteHandler(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "Complete todo")
-	log.Printf("INFO: mark todo complete is hit\n")
+func MarkTodoCompleteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	todo_id := vars["todo"]
+
+	stmt, err := db.Prepare(`UPDATE todos SET is_completed = true WHERE id = ?`)
+	if err != nil {
+		log.Fatalf("ERROR: could not prepare query: %s", err.Error())
+	}
+	defer stmt.Close()
+
+	_, query_err := stmt.Exec(todo_id)
+	if query_err != nil {
+		log.Fatalf("ERROR: could not mark todo as complete: %s", err.Error())
+	}
+
+	response := Response{Message: "Todo marked as completed successfully."}
+	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
 	var db_err error
 	db, db_err = sql.Open("sqlite3", "./gotodo.db")
 	if db_err != nil {
-		log.Fatalf("ERROR: cannot connect to database: %v", db_err.Error())
+		log.Fatalf("ERROR: cannot connect to database: %s", db_err.Error())
 	}
 	defer db.Close()
+
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS todos (id STRING NOT NULL PRIMARY KEY, todo TEXT, is_completed BOOLEAN DEFAULT FALSE);
+	`
+	_, err := db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatalf("ERROR: could not create todos table: %s\n", err.Error())
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/api/todos", GetTodoHandler).Methods("GET")
 	router.HandleFunc("/api/todos", AddTodoHandler).Methods("POST")
 	router.HandleFunc("/api/todos/{todo}", UpdateTodoHandler).Methods("PUT")
 	router.HandleFunc("/api/todos/{todo}", DeleteTodoHandler).Methods("DELETE")
-	router.HandleFunc("/api/todos/{todo}", MarkTodoCompleteHandler).Methods("PATCH")
+	router.HandleFunc("/api/todos/{todo}/complete", MarkTodoCompleteHandler).Methods("PATCH")
 
-	log.Printf("INFO: server is listening on port: %v\n", PORT)
+	log.Printf("INFO: server is listening on port: %s\n", PORT)
 
-	err := http.ListenAndServe(":"+PORT, router)
+	err = http.ListenAndServe(":"+PORT, router)
 	if err != nil {
-		log.Fatalf("ERROR: Could not start the server on port: %v\n", PORT)
+		log.Fatalf("ERROR: Could not start the server on port: %s\n", PORT)
 	}
 }
